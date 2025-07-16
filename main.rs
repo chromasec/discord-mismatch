8b5e8e0f37ecf919f6026911bde25c9de6d520af
chromasec <110331414+chromasec@users.noreply.github.com>
2025-07-16 11:14:40 +0200

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,27 @@
+fn uniggerify(corrupted: &str) -> String {
+    let utf16_units: Vec<u16> = corrupted.encode_utf16().collect();
+
+    let mut digits = String::new();
+
+    for unit in utf16_units {
+        let high = (unit >> 8) as u8;
+        let low = (unit & 0xFF) as u8;
+        if (0x30..=0x39).contains(&high) {
+            digits.push(high as char);
+        }
+        else if (0x30..=0x39).contains(&low) {
+            digits.push(low as char);
+        }
+        else {
+            digits.push('?');
+        }
+    }
+
+    digits
+}
+
+fn main() {
+    let corrupted = "嘀漀琀爀攀 挀漀搀攀 搀攀 猀挀甀爀椀琀 䐀椀猀挀漀爀搀 攀猀琀ꀀ㨀 㘀㜀㘀㤀㤀　";
+    let decoded = uniggerify(corrupted);
+    println!("Decoded digits: {}", decoded);
+}
